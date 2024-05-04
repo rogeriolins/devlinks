631381776f216aa61987bb50492f822cf95974ee
Rogerio Lins <rogerio_lins2002@yahoo.com>
2024-05-04 17:48:06 -0300

    firts commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function onToggleMode() {
+  // Se fosse no html
+  // const html = document.documentElement
+  const body = document.body
+  // if (body.classList.contains("light-mode")) {
+  //   body.classList.remove("light-mode")
+  // } else {
+  //   body.classList.add("light-mode")
+  // }
+  body.classList.toggle("light-mode")
+
+  // Pegamos a tag img e trocamos a imagem
+  const img = document.querySelector("#avatar img")
+  if (body.classList.contains("light-mode")) {
+    // Light Mode
+    img
+      .setAttribute("src", "./assets/my-avatar-light.png")
+      .setAttribute(
+        "alt",
+        "Foto Rogério Lins com fundo verde, camiseta preta, de oculos, barba e mão no queixo, sério e sem sorrir."
+      )
+  } else {
+    img
+      .setAttribute("src", "./assets/my-avatar.png")
+      .setAttribute(
+        "alt",
+        "Foto Rogério Lins com fundo verde, camiseta preta, de oculos e barba sorrindo."
+      )
+  }
+}
